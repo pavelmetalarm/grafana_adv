2c3d080e4586ba61c3162ccb4d90af81fd0a4b08
pavelmetalarm <49231300+pavelmetalarm@users.noreply.github.com>
2020-08-28 17:26:32 +0300

    scripted.js
    .

---
+++ scripted.js
@@ -0,0 +1,56 @@
+// time can be overridden in the url using from/to parameters, but this is
+// handled automatically in grafana core during dashboard initialization
+dashboard.time = {
+  from: "now-6h",
+  to: "now"
+};
+
+var rows = 1;
+var seriesName = 'argName';
+
+if(!_.isUndefined(ARGS.rows)) {
+  rows = parseInt(ARGS.rows, 10);
+}
+
+if(!_.isUndefined(ARGS.name)) {
+  seriesName = ARGS.name;
+}
+
+for (var i = 0; i < rows; i++) {
+
+  dashboard.rows.push({
+    title: 'Chart',
+    height: '300px',
+    panels: [
+      {
+        title: 'Events',
+        type: 'graph',
+        span: 12,
+        fill: 1,
+        linewidth: 2,
+        targets: [
+          {
+            'target': "randomWalk('" + seriesName + "')"
+          },
+          {
+            'target': "randomWalk('random walk2')"
+          }
+        ],
+        seriesOverrides: [
+          {
+            alias: '/random/',
+            yaxis: 2,
+            fill: 0,
+            linewidth: 5
+          }
+        ],
+        tooltip: {
+          shared: true
+        }
+      }
+    ]
+  });
+}
+
+
+return dashboard;
